8f02c1086b848d68332c398814e09b924166b956
Vikas Parmar <36152918+visparmar@users.noreply.github.com>
2022-06-05 01:54:42 +0530

    Add files via upload

---
+++ majority element.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+int findMajorityElement(int arr[], int n) {
+	 int  candidate = -1, votes = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (votes == 0) {
+            candidate = arr[i];
+            votes = 1;
+        }
+        else {
+            if (arr[i] == candidate)
+                votes++;
+            else
+                votes--;
+        }
+    }
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == candidate)
+            count++;
+    }
+
+    if (count > n / 2)
+        return candidate;
+    return -1;
+}
