cea4a638e213b6fe28526bd6ce4bc5ab6dea5377
Vikas Parmar <36152918+visparmar@users.noreply.github.com>
2022-12-30 15:02:55 +0530

    Add another Method

--- Pascal's_Triangle.cpp
+++ Pascal's_Triangle.cpp
@@ -20,3 +20,32 @@ vector<vector<long long int>> printPascal(int n)
    return tri;
 
 }
+
+// another Method......
+
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {         
+        vector<vector<int>> ans;
+           for(int i=0;i<numRows;i++){
+                   vector<int> temp;
+                       for(int j=0;j<=i;j++){
+          
+                        if(j==0 || j==i){
+                         temp.push_back(1);
+                         }
+                        else{
+                        int sum=ans[i-1][j]+ans[i-1][j-1];
+                        temp.push_back(sum);
+
+                          }
+
+
+                        }
+                    ans.push_back(temp);
+                         }
+                     return ans;
+        
+                        }
+};
